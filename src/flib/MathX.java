c252c7324e416df752d69470adbfabfb13a9327e
Freeman <t961735889@163.com>
2023-06-06 19:05:23 +0800

    Add MathX.java

---
+++ src/flib/MathX.java
@@ -0,0 +1,56 @@
+package flib;
+
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MathX {
+
+    // ============================== 常用数学函数 ==============================
+
+    /**
+     * 高斯函数
+     *
+     * @param x     自变量
+     * @param sigma 标准差
+     * @param mu    数学期望
+     * @return 高斯函数值
+     */
+    public static double gauss(double x, double sigma, double mu) {
+        double coefficient = 1 / (sigma * Math.sqrt(2 * Math.PI));
+        double exp = -Math.pow(x - mu, 2) / (2 * Math.pow(sigma, 2));
+        return coefficient * Math.exp(exp);
+    }
+
+    /**
+     * 默认数学期望为0的高斯函数。
+     *
+     * @param x     自变量
+     * @param sigma 标准差
+     * @return 高斯函数值
+     */
+    public static double gauss(double x, double sigma) {
+        return gauss(x, sigma, 0);
+    }
+
+    /**
+     * 默认标准差为1，数学期望为0的高斯函数（标准正态分布）。
+     *
+     * @param x 自变量
+     * @return 高斯函数值
+     */
+    public static double gauss(double x) {
+        return gauss(x, 1, 0);
+    }
+
+    /**
+     * 返回以2为底，x的对数
+     *
+     * @param x 自变量
+     * @return 以2为底，x的对数
+     */
+    public static double log2(double x) {
+        return Math.log(x) / Math.log(2);
+    }
+}
